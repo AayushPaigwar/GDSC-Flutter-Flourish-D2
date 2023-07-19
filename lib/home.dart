1315837c6368a2273de45b817896d8ecfaa2baf8
Aayush <aayush.paigwar123@gmail.com>
2023-07-19 19:24:42 +0530

    first API call

---
+++ lib/home.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:learning_apis/model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<SamplePosts> samplePosts = [];
+
+  @override
+  Widget build(BuildContext context) {
+    //future builder shows a "loading" option till the data gets from the server. It just imporves the UX.
+    return FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: samplePosts.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 130,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'User id: ${samplePosts[index].userId}',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'ID: ${samplePosts[index].id}',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Title: ${samplePosts[index].title}',
+                        maxLines: 1,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Body: ${samplePosts[index].body}',
+                        maxLines: 1,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
+  }
+
+  Future<List<SamplePosts>> getData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> index in data) {
+        samplePosts.add(SamplePosts.fromJson(index));
+      }
+      return samplePosts;
+    } else {
+      return samplePosts;
+    }
+  }
+}
